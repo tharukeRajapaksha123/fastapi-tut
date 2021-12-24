84e93341c558364ab5edd8030bf84b67d875b56c
tharukeRajapaksha123 <darksparrow156@gmail.com>
2021-12-24 09:25:34 +0530

    ADD:ready to deploy

--- handle_files.py
+++ handle_files.py
@@ -5,6 +5,11 @@ from fastapi.param_functions import File
 app = FastAPI() 
 
 
+@app.get("/")
+def hello():
+    return {"workstatys" : "fine"}
+
+
 @app.post("/get-file") 
 async def root(file:UploadFile = File(...)):
     return {"file_name" : file.filename}
